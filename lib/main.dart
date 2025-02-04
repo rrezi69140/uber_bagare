1b29ee7b1577ec738b364a0ab5573d895d0f7c24
Rezig Rachid <rachid.rezig@enfrasys.fr>
2025-02-04 18:43:08 +0100

    ajout bouton sous image pour la navigation

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
 
 Future<List<dynamic>> getProfilePIctures() async {
   // Construction de l'URL a appeler
-  var url = Uri.parse("https://randomuser.me/api/?results=8");
+  var url = Uri.parse("https://randomuser.me/api/?results=8"); // utilisation de l'api random user pour génerer les information  des utilisateur 
   // Appel
   var response = await http.get(url);
   if (response.statusCode == 200) {
@@ -62,17 +62,20 @@ class MyApp extends StatelessWidget {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           
+                          
                           spacing: 15,
                           children: [
-                            CircleAvatar(
+                            ElevatedButton(onPressed: (){}, child:  CircleAvatar(
                               backgroundImage:
                                   NetworkImage("${bagareuer['picture']['medium']}"),
                        
-                            ),
+                            ),),
+                           
                             Column(
+                              
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Text(" nom : ${bagareuer['name']['first']}  ${bagareuer['name']['last']}   "),
+                                Text(" nom : ${bagareuer['name']['first']}  ${bagareuer['name']['last']}   "), // crée des valeur fictive 
                                 Text(
                                     "${bagareuer['dob']['age']/2.toInt()} victoire -- ${bagareuer['dob']['age']%2.toInt()} defaite -- ${bagareuer['dob']['age']/10.toInt()} KO")
                               ],
