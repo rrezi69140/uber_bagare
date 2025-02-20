7a8b5c635cab52e3154e1cea6e411b8aac3a46ff
Rezig rachid <rachid.rezig69@gmail.com>
2025-02-20 15:16:38 +0100

    Ajout system de provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'dart:convert';
-import 'package:uber_bagare/Pages/Home.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
-import 'package:http/http.dart' as http;
-import 'package:uber_bagare/Pages/FighterProfile.dart';
+import 'package:provider/provider.dart';
+import 'package:uber_bagare/Pages/Home.dart';
 
+import 'Pages/MapView.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   MapboxOptions.setAccessToken(dotenv.get('MAPBOX_ACCESS_TOKEN'));
-  runApp(MaterialApp(
-    title: "UberBagarre",
-    home: MyApp(),
-  ));
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => PositionProvider(),
+      child: MaterialApp(
+        title: "UberBagarre",
+        home: MyApp(),
+      ),
+    ),
+  );
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
-
   Widget build(BuildContext context) {
-    return  Home();
+    return Home();
   }
 }
