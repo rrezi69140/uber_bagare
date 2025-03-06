029afc02aaa13dcdb7091c4a15b5fa0b96e75b39
Rezig rachid <rachid.rezig69@gmail.com>
2025-03-06 18:20:55 +0100

    Correction ajout user dans supabase

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,4 +1,3 @@
-
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
@@ -10,48 +9,26 @@ import '../model/position_model.dart';
 import '../services/fighter_service.dart';
 
 Future<void> updateUserInDatabase() async {
+  SupabaseClient supabase = Supabase.instance.client;
+  print(Supabase.instance.client);
 
-
-
-
-
-
-
-    SupabaseClient supabase = Supabase.instance.client;
   final user = supabase.auth.currentUser;
   if (user != null) {
-     try {
-     var position = await ModelPosition.determinePosition();
-    await supabase.from('users').upsert({
-      'id': user.id,
-      'Prenom': user.userMetadata?['full_name'],
-      'Nom': user.userMetadata?['last_name'],
-       'email': user.email,
-      'avatar_url': user.userMetadata?['avatar_url'],
-       'Victory': user.email,
-        'defeat': user.email,
-         'Longitude': position.longitude,
-         'latitude':  position.latitude,
-         'strengh': 0,
-         'agility': 0,
-         
-
-
-         'inteligence': 0,
-          'ko': 0,
-         
-    
-         
-
-    });
+    try {
+      var position = await ModelPosition.determinePosition();
+      await supabase.from('users').upsert({
+        'id': user.id, // Doit être un UUID
+        'first_name': user.userMetadata?['full_name'],
+        'last_name': user.userMetadata?['last_name'],
+        'email': user.email,
+        'avatar_url': user.userMetadata?['avatar_url'],
+        'longitude': position.longitude,
+        'latitude': position.latitude,
+      });
 
       print("Utilisateur mis à jour avec succès dans Supabase !");
-     }
-     catch (e) {
+    } catch (e) {
       print("Erreur lors de la mise à jour de l'utilisateur : $e");
     }
   }
 }
-
-
- 
